ea92ba3326ee14db725498c73acc29e6344b63df
영현 <yh97yhyh@naver.com>
2024-08-05 22:28:56 +0900

    first commit

---
+++ trip-companions/trip_companionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  trip_companionsApp.swift
+//  trip-companions
+//
+//  Created by 영현 on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct trip_companionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
