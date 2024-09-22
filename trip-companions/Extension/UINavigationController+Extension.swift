072a086cf7d81bbcbafee1175e49fa62662dbab0
영현 <yh97yhyh@naver.com>
2024-09-22 20:46:27 +0900

    feat: Add assets

---
+++ trip-companions/Extension/UINavigationController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+Extension.swift
+//  trip-companions
+//
+//  Created by 영현 on 9/22/24.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
